3e182688c70831afcbae0336c2b4f95495e01eea
Sandip Dutta <57990650+Dutta-SD@users.noreply.github.com>
2020-04-09 19:27:01 +0530

    Update and rename file
    Updated Documentation

--- Fast_Exponentiation_GCD.py
+++ Fast_Exponentiation_GCD.py
@@ -11,12 +11,15 @@ Created on Mon Apr  6 09:41:49 2020
 # =============================================================================
 
 def Bin_Expo(a, n):
+    # if the exponent is 0, return 1(Base Case)
     if n == 0:
         return 1
-    elif n > 0:
+    else:       
         if n & 1:
+             # If the exponent is odd ,then call Bin_Expo with (a^(n-1)/2)**2 * a
             return Bin_Expo(a, (n - 1) >> 1) * Bin_Expo(a, (n - 1) >> 1) * a
         else:
+            # Call the Bin_Expo with (a^(n/2))*2
             return Bin_Expo(a, n >> 1) * Bin_Expo(a, n >> 1)
         
 # =============================================================================
